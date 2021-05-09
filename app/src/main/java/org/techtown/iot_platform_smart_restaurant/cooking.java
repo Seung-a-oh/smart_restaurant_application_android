17809deaafadd4bc62172ea12febd8f0e1665549
Seung-a-oh <osa0215@naver.com>
2021-05-10 02:12:57 +0900

    no message

--- app/src/main/java/org/techtown/iot_platform_smart_restaurant/cooking.java
+++ app/src/main/java/org/techtown/iot_platform_smart_restaurant/cooking.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class cooking extends AppCompatActivity {
+public class Cooking extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
