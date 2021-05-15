a2b6150e89d852a74303f6d67dbd7df71c05ffc0
Seung-a-oh <osa0215@naver.com>
2021-05-16 03:24:32 +0900

    좌석의 선택 예약 여부를 저장하는 YesOrNo라는 변수를 만들어 저장함. 선택한 좌석 번호를 booking과 공유 할 수 있도록 함.

--- app/src/main/java/org/techtown/iot_platform_smart_restaurant/realtime_seats.java
+++ app/src/main/java/org/techtown/iot_platform_smart_restaurant/realtime_seats.java
@@ -3,6 +3,7 @@ package org.techtown.iot_platform_smart_restaurant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
@@ -11,11 +12,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class Realtime_seats extends AppCompatActivity {
-    public int seat_number;
+    static int seat_number;
+    static int[] YesOrNo = new int[7];
 
     public void showToast(String data){
         Toast.makeText(this, data, Toast.LENGTH_LONG).show();
     }
+
+    // [코드 작성] 설정 시간(t)이 지나고, 테이블의 이용이 없다면 예약 가능하게 변경.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,40 +27,51 @@ public class Realtime_seats extends AppCompatActivity {
         setContentView(R.layout.realtime_seats);
 
         //잔여석 표시하는 코드
-        TextView available = (TextView)findViewById(R.id.textView2);
-        available.setText("0"); //서버에서 받아온 값으로 수정해야함.
+        TextView available = findViewById(R.id.textView2);
+        available.setText("16"); //서버에서 받아온 값으로 수정해야함.
 
-        Button button1 = (Button)findViewById(R.id.button1);
+        final Button button1 = findViewById(R.id.button1);
+
         button1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 seat_number=1;
+                if (YesOrNo[seat_number] == 1){
+                    button1.setEnabled(false);
+                    return;
+                }
                 Intent intent = new Intent(Realtime_seats.this,
                         Booking.class);
                 startActivity(intent);
-                //1번이 클릭됐을때 전역변수 seat_number에 현재 선택한 좌석을 넣어놓고
-//                booking에서 seat_number를 출력한다. -> 전역 변수 seat_number를 만든 뒤 상속.
             }
         });
 
+        final Button button2 = (Button)findViewById(R.id.button2);
 
-        Button button2 = (Button)findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 seat_number = 2;
+                if (YesOrNo[seat_number] == 1){
+                    button2.setEnabled(false);
+                    return;
+                }
                 Intent intent = new Intent(Realtime_seats.this,
                         Booking.class);
                 startActivity(intent);
+
             }
         });
 
-
-        Button button3 = (Button)findViewById(R.id.button3);
+        final Button button3 = (Button)findViewById(R.id.button3);
         button3.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 seat_number = 3;
+                if (YesOrNo[seat_number] == 1){
+                    button3.setEnabled(false);
+                    return;
+                }
                 Intent intent = new Intent(Realtime_seats.this,
                         Booking.class);
                 startActivity(intent);
@@ -64,23 +79,32 @@ public class Realtime_seats extends AppCompatActivity {
         });
 
 
-        Button button4 = (Button)findViewById(R.id.button4);
+        final Button button4 = (Button)findViewById(R.id.button4);
         button4.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 seat_number = 4;
+                if (YesOrNo[seat_number] == 1){
+                    button4.setEnabled(false);
+                    return;
+                }
                 Intent intent = new Intent(Realtime_seats.this,
                         Booking.class);
                 startActivity(intent);
+
             }
         });
 
 
-        Button button5 = (Button)findViewById(R.id.button5);
+        final Button button5 = (Button)findViewById(R.id.button5);
         button5.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 seat_number = 5;
+                if (YesOrNo[seat_number] == 1){
+                    button5.setEnabled(false);
+                    return;
+                }
                 Intent intent = new Intent(Realtime_seats.this,
                         Booking.class);
                 startActivity(intent);
@@ -88,17 +112,19 @@ public class Realtime_seats extends AppCompatActivity {
         });
 
 
-        Button button6 = (Button)findViewById(R.id.button6);
+        final Button button6 = (Button)findViewById(R.id.button6);
         button6.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 seat_number = 6;
+                if (YesOrNo[seat_number] == 1){
+                    button6.setEnabled(false);
+                    return;
+                }
                 Intent intent = new Intent(Realtime_seats.this,
                         Booking.class);
                 startActivity(intent);
             }
         });
     }
-
-
 }
